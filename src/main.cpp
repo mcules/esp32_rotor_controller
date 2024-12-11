f76491a123628b399ad18d8233ad855d31a373f2
McUles <github@xxxxxxxx.xyz>
2024-12-12 00:02:48 +0100

    Protocol working for multiple clients

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 WiFiManager wifiManager;
 
 #define TCP_SERVER_PORT 4533
+#define POSITION_UPDATE_INTERVAL 10 // Position update in ms
 
 double currentAzimuth = 0.0;
 double currentElevation = 0.0;
@@ -12,136 +13,158 @@ double currentElevation = 0.0;
 double targetAzimuth = 0.0;
 double targetElevation = 0.0;
 
-char delimiter = ' ';
-
 WiFiServer server(TCP_SERVER_PORT);
 
-void splitString(String input, char delimiter, std::vector<String> &output)
+unsigned long lastPositionUpdate = 0;
+std::vector<WiFiClient> clients;
+
+void splitString(const String &input, std::vector<String> &output)
 {
     int start = 0;
-
-    while (true)
+    int end;
+    while ((end = input.indexOf(' ', start)) != -1)
     {
-        int end = input.indexOf(delimiter, start);
-
-        if (end == -1)
-        {
-            output.push_back(input.substring(start));
-            break;
-        }
-        else
-        {
-            output.push_back(input.substring(start, end));
-            start = end + 1;
-        }
+        output.push_back(input.substring(start, end));
+        start = end + 1;
     }
+    output.push_back(input.substring(start));
 }
 
 static String getRotorCurrentPosition()
 {
-    Serial.printf("get_pos %f %f\n", currentAzimuth, currentElevation);
-
-    return String(currentAzimuth, 2) + "\n" + String(currentElevation, 2) + "\n";
+    return String(currentAzimuth, 2) + "\n" + String(currentElevation, 2) + "\nRPRT 0\n";
 }
 
 static String setRotorPosition(double azimuth, double elevation)
 {
     targetAzimuth = azimuth;
     targetElevation = elevation;
-
-    Serial.printf("set_pos %f %f\n", azimuth, elevation);
-
     return "RPRT 0\n";
 }
 
 static String stopRotor()
 {
-    // TODO: needs implementation
+    targetAzimuth = currentAzimuth;
+    targetElevation = currentElevation;
+
     return "RPRT 0\n";
+}
+
+static String getDumpState()
+{
+    return "min_az=-180.00\nmax_az=180.00\nmin_el=-90.00\nmax_el=90.00\nsouth_zero=10.00\nRPRT 0\n";
+}
+
+void processCommand(String command, WiFiClient &client)
+{
+    String toSendString = "";
+    std::vector<String> output;
+
+    command.trim();
+    if (!command.isEmpty())
+    {
+        Serial.println("Received command: " + command);
+        splitString(command, output);
+
+        if (output[0] == "p")
+        {
+            toSendString = getRotorCurrentPosition();
+        }
+        else if (output[0] == "P")
+        {
+            if (output.size() == 3)
+            {
+                output[1].replace(',', '.');
+                output[2].replace(',', '.');
+                toSendString = setRotorPosition(output[1].toDouble(), output[2].toDouble());
+            }
+            else
+            {
+                toSendString = "ERR Invalid command length\nRPRT -8\n";
+            }
+        }
+        else if (output[0] == "S")
+        {
+            toSendString = stopRotor();
+        }
+        else if (output[0] == "_")
+        {
+            toSendString = "Model Name: ESP32 Rotor Controller Az/El\nRPRT 0\n";
+        }
+        else if (output[0] == "q")
+        {
+            client.stop();
+            return;
+        }
+        else if (output[0] == "\\dump_state")
+        {
+            toSendString = getDumpState();
+        }
+        else
+        {
+            toSendString = "ERR Unknown command\nRPRT -1\n";
+        }
+
+        if (client.connected())
+        {
+            client.print(toSendString);
+        }
+    }
+}
+
+void updatePosition()
+{
+    if (millis() - lastPositionUpdate >= POSITION_UPDATE_INTERVAL)
+    {
+        lastPositionUpdate = millis();
+
+        if (targetAzimuth > currentAzimuth)
+            currentAzimuth += 0.1;
+        if (targetAzimuth < currentAzimuth)
+            currentAzimuth -= 0.1;
+
+        if (targetElevation > currentElevation)
+            currentElevation += 0.1;
+        if (targetElevation < currentElevation)
+            currentElevation -= 0.1;
+    }
+}
+
+void handleClients()
+{
+    WiFiClient newClient = server.available();
+    if (newClient)
+    {
+        clients.push_back(newClient);
+    }
+
+    for (int i = clients.size() - 1; i >= 0; i--)
+    {
+        WiFiClient &client = clients[i];
+        if (!client.connected())
+        {
+            clients.erase(clients.begin() + i);
+            continue;
+        }
+
+        if (client.available() > 0)
+        {
+            String command = client.readStringUntil('\n');
+            processCommand(command, client);
+        }
+    }
 }
 
 void setup()
 {
     Serial.begin(115200);
-
     wifiManager.autoConnect();
-
     server.begin();
-    Serial.printf("\n Server started on Port %d\n", TCP_SERVER_PORT);
-}
-
-void handleClient(WiFiClient client)
-{
-    while (client.connected())
-    {
-        String command = client.readStringUntil('\n');
-        command.trim();
-
-        std::vector<String> output;
-        splitString(command, delimiter, output);
-
-        String toSendString = "";
-
-        if (output[0] == "p" || output[0] == "get_pos")
-        {
-            toSendString = getRotorCurrentPosition();
-        }
-        else if (output[0] == "P" || output[0] == "set_pos")
-        {
-            if (output.size() == 3)
-            {
-                toSendString = setRotorPosition(output[1].toDouble(), output[2].toDouble());
-            }
-            else
-            {
-                toSendString = "ERR Invalid command length\n";
-            }
-        }
-        else if (output[0] == "S" || output[0] == "stop")
-        {
-            toSendString = stopRotor();
-        }
-        else if (output[0] == "K" || output[0] == "park")
-        {
-            setRotorPosition(0.0, 0.0);
-        }
-
-        Serial.println(toSendString);
-
-        if (client.connected())
-        {
-            client.println(toSendString);
-        }
-    }
-    client.stop();
-    Serial.println("Client disconnected");
+    Serial.printf("\nServer started on Port %d\n", TCP_SERVER_PORT);
 }
 
 void loop()
 {
-    WiFiClient client = server.available();
-
-    if (client)
-    {
-        Serial.println("New client connected");
-        handleClient(client);
-    }
-
-    if (targetAzimuth > currentAzimuth)
-    {
-        currentAzimuth += 0.00005;
-    }
-    else if (targetAzimuth < currentAzimuth)
-    {
-        currentAzimuth -= 0.00005;
-    }
-
-    if (targetElevation > currentElevation)
-    {
-        currentElevation += 0.00005;
-    }
-    else if (targetElevation < currentElevation)
-    {
-        currentElevation -= 0.00005;
-    }
+    updatePosition();
+    handleClients();
 }
